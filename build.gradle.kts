a0dadcbf62662238b05ad09c7d54061a97adf954
hiagobalbino <hiagobalbinoo@gmail.com>
2023-11-26 19:06:08 -0300

    :bulb: raw: configure flyway to run on boostrap

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
-//	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.flywaydb:flyway-core")
 
 	runtimeOnly("org.postgresql:postgresql")
--- src/main/resources/db/migration/V1__criar_tabela_clientes.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE clientes (
-  id SERIAL PRIMARY KEY,
-  nome VARCHAR(50),
-  email VARCHAR(100)
-);
