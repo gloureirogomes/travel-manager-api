77d5e3befa643ef9886fee3b758cbeef1e3c2366
gloureirogomes <loureirobiel@gmail.com>
2023-11-27 21:47:38 -0300

    :bulb: chore: create initial migration
    Co-authored-by: @hiago-balbino <hiagobalbinoo@gmail.com>
    Co-authored-by: @gloureirogomes <loureirobiel@gmail.com>

---
+++ src/main/resources/db/migration/V1__create_initial_tables.sql
@@ -0,0 +1,36 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE address (
+  address_id uuid NOT NULL DEFAULT uuid_generate_v4(),
+  street VARCHAR(100) NOT NULL,
+  city VARCHAR(50) NOT NULL,
+  neighborhood VARCHAR(50),
+  zip_code VARCHAR(50) NOT NULL,
+  country VARCHAR(50) NOT NULL,
+  PRIMARY KEY(address_id)
+);
+
+CREATE TABLE users (
+  user_id uuid NOT NULL DEFAULT uuid_generate_v4(),
+  name VARCHAR(150) NOT NULL,
+  email VARCHAR(100) UNIQUE NOT NULL,
+  password VARCHAR(300) NOT NULL,
+  active BOOLEAN NOT NULL,
+  updated_at TIMESTAMP NOT NULL DEFAULT now(),
+  created_at TIMESTAMP NOT NULL DEFAULT now(),
+  address_id uuid,
+  PRIMARY KEY(user_id),
+  CONSTRAINT fk_address FOREIGN KEY(address_id) REFERENCES address(address_id)
+);
+
+CREATE TABLE travels (
+  travel_id uuid NOT NULL DEFAULT uuid_generate_v4(),
+  order_code VARCHAR(30) NOT NULL,
+  amount MONEY NOT NULL,
+  start_date TIMESTAMP NOT NULL,
+  end_date TIMESTAMP NULL,
+  type VARCHAR(15) NOT NULL,
+  user_id uuid NOT NULL,
+  PRIMARY KEY(travel_id),
+  CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES users(user_id)
+);
