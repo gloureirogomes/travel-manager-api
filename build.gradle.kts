9762dc7b5b0de5f0df29ba64d7892f2fb92528f1
gloureirogomes <loureirobiel@gmail.com>
2023-12-06 02:12:29 -0300

    :heavy_plus_sign: chore: setting versions for non spring dependencies
    Co-authored-by: @gloureirogomes <loureirobiel@gmail.com>
    Co-authored-by: @hiago-balbino <hiagobalbinoo@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,12 @@ plugins {
 group = "com.tma"
 version = "0.0.1-SNAPSHOT"
 
+val postgresVersion = "42.7.0"
+val lombokVersion = "1.18.30"
+val junitVersion = "5.10.1"
+val postgreTestContainerVersion = "1.19.3"
+val flywayVersion = "10.1.0"
+
 java {
 	sourceCompatibility = JavaVersion.VERSION_19
 }
@@ -28,15 +34,16 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 //	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 
-	runtimeOnly("org.postgresql:postgresql")
-	implementation("org.flywaydb:flyway-core")
-	compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
+	runtimeOnly("org.postgresql:postgresql:$postgresVersion")
+	runtimeOnly("org.flywaydb:flyway-database-postgresql:$flywayVersion")
+	implementation("org.flywaydb:flyway-core:$flywayVersion")
+	compileOnly("org.projectlombok:lombok:$lombokVersion")
+	annotationProcessor("org.projectlombok:lombok:$lombokVersion")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
-	testImplementation("org.testcontainers:junit-jupiter")
-	testImplementation("org.testcontainers:postgresql")
+	testImplementation("org.testcontainers:junit-jupiter:$junitVersion")
+	testImplementation("org.testcontainers:postgresql:$postgreTestContainerVersion")
 }
 
 tasks.withType<Test> {
