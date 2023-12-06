db463fd854c6701397cd29aa1d4f6e8b5df37e2d
gloureirogomes <loureirobiel@gmail.com>
2023-12-06 01:29:06 -0300

    :white_check_mark: test: create integration test configuration
    Co-authored-by: @gloureirogomes <loureirobiel@gmail.com>
    Co-authored-by: @hiago-balbino <hiagobalbinoo@gmail.com>

--- src/test/java/com/tma/travelmanagerapi/repository/UserRepositoryIntegrationTest.java
+++ src/test/java/com/tma/travelmanagerapi/repository/UserRepositoryIntegrationTest.java
@@ -1,40 +1,26 @@
 package com.tma.travelmanagerapi.repository;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
+import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataJpaTest
-@Import(UserRepository.class)
 @Testcontainers
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserRepositoryIntegrationTest {
-
-    @Container
-    private static final PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:latest").
-            withUsername("postgres").
-            withPassword("postgres").
-            withDatabaseName("travel-manager");
 
     @Autowired
     private UserRepository userRepository;
 
-    @DynamicPropertySource
-    static void postgresqlProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
-        registry.add("spring.datasource.username", postgresContainer::getUsername);
-        registry.add("spring.datasource.password", postgresContainer::getPassword);
-    }
-
-   @Test
+    @Test
+    @DisplayName("Test Anything")
     public void testAnything() {
         assertEquals(0, userRepository.count());
    }
---
+++ src/test/resources/db/migration/V1__create_initial_tables.sql
@@ -0,0 +1,31 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users (
+  user_id uuid NOT NULL DEFAULT uuid_generate_v4(),
+  name VARCHAR(150) NOT NULL,
+  email VARCHAR(100) UNIQUE NOT NULL,
+  password VARCHAR(300) NOT NULL,
+  active BOOLEAN NOT NULL DEFAULT TRUE,
+  street VARCHAR(100) NOT NULL,
+  city VARCHAR(50) NOT NULL,
+  neighborhood VARCHAR(50),
+  zip_code VARCHAR(50) NOT NULL,
+  country VARCHAR(50) NOT NULL,
+  updated_at TIMESTAMP NOT NULL DEFAULT now(),
+  created_at TIMESTAMP NOT NULL DEFAULT now(),
+  PRIMARY KEY(user_id)
+);
+
+CREATE TABLE travels (
+  travel_id uuid NOT NULL DEFAULT uuid_generate_v4(),
+  order_code VARCHAR(30) NOT NULL,
+  amount MONEY NOT NULL,
+  start_date TIMESTAMP NOT NULL,
+  end_date TIMESTAMP NULL,
+  type VARCHAR(15) NOT NULL,
+  user_id uuid NOT NULL,
+  updated_at TIMESTAMP NOT NULL DEFAULT now(),
+  created_at TIMESTAMP NOT NULL DEFAULT now(),
+  PRIMARY KEY(travel_id),
+  CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES users(user_id)
+);
